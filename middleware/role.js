56f219be3450db33f72bffed75b1cc68a67412ca
nibles1998 <longvynguyenhoang@gmail.com>
2020-05-17 12:21:32 +0700

    Fix check input Task Role Middleware

--- middleware/role.js
+++ middleware/role.js
@@ -18,38 +18,17 @@ role.checkRole = function (roleType, roleOption) {
                 });
             }
         }
-
-        if (Object.keys(roleOption).length > 0 && roleOption !== null && roleOption !== undefined) {
+        const roleKey = Object.keys(roleOption);
+        if (roleKey.length > 0 && roleOption !== null && roleOption !== undefined) {
             let checkOption = true;
-            const key = Object.keys(roleOption);
-            for (let item of key) {
-                if (item === "create") {
-                    if (roleOption[item] !== userRole.create) {
-                        checkOption = false;
-                    }
+            for (let item of roleKey) {
+                if (userRole[item] !== roleOption[item]) {
+                    checkOption = false;
+                    return res.status(400).json({
+                        success: false,
+                        message: "Bạn không được cấp quyền sử dụng chức năng này!"
+                    });
                 }
-                if (item === "read") {
-                    if (roleOption[item] !== userRole.read) {
-                        checkOption = false;
-                    }
-                }
-                if (item === "update") {
-                    if (roleOption[item] !== userRole.update) {
-                        checkOption = false;
-                    }
-                }
-                if (item === "delete") {
-                    if (roleOption[item] !== userRole.delete) {
-                        checkOption = false;
-                    }
-                }
-            }
-
-            if (!checkOption) {
-                return res.status(400).json({
-                    success: false,
-                    message: "Bạn không được cấp quyền sử dụng chức năng này!"
-                });
             }
         }
         next();
