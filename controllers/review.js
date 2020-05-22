65cbd9846a4c9104cbd9ca414a3d2d04e67a9746
nibles1998 <longvynguyenhoang@gmail.com>
2020-05-22 09:12:57 +0700

    [Update] Task [Review] Controllers: Add filter for getMany

--- controllers/review.js
+++ controllers/review.js
@@ -4,7 +4,27 @@ const reviewCtrl = {};
 
 reviewCtrl.getMany = async function (req, res, next) {
     try {
-        const review = await reviewModel.find();
+        const query = req.query;
+        const whereQuery = {};
+        const sortQuery = {};
+        const allKeys = Object.keys(query);
+        for (let index = 0; index < allKeys.length; index++) {
+            const _queryKey = allKeys[index];
+            if (_queryKey == "userId") {
+                whereQuery.userId = query[_queryKey];
+                continue;
+            }
+            if (_queryKey == "tourId") {
+                whereQuery.tourId = query[_queryKey];
+                continue;
+            }
+            if (_queryKey == "sortrating") {
+                sortQuery["rating"] = query[_queryKey];
+                continue;
+            }
+        }
+
+        const review = await reviewModel.find(whereQuery).sort(sortQuery);
         res.status(200).json({
             success: true,
             data: review
