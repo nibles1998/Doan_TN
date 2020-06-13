c1bc90588df2fdf1726d718775cf7c2c89022448
nibles1998 <longvynguyenhoang@gmail.com>
2020-06-13 07:55:06 +0700

    [Update] Add role when put for Task [Bill] Route

--- routes/bill.js
+++ routes/bill.js
@@ -5,8 +5,8 @@ const role = require('../middleware/role');
 
 billRoute.get('/s', authenticate.authenticateJWT, role.checkRole(["Admin", "Customer"], { read: true }), billCtrl.getMany);
 billRoute.get('/:_id', authenticate.authenticateJWT, role.checkRole(["Admin", "Customer"], { read: true }), billCtrl.getById);
-billRoute.post('/', authenticate.authenticateJWT, role.checkRole(["Admin"], { create: true }), billCtrl.createData);
-billRoute.put('/:_id', authenticate.authenticateJWT, role.checkRole(["Admin"], { up: true }), billCtrl.updateById);
+billRoute.post('/', authenticate.authenticateJWT, role.checkRole(["Admin", "Customer"], { create: true }), billCtrl.createData);
+billRoute.put('/:_id', authenticate.authenticateJWT, role.checkRole(["Admin", "Customer"], { up: true }), billCtrl.updateById);
 // billRoute.delete('/:id', billCtrl.deleteById);
 
 module.exports = billRoute;
