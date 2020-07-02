b1d3a7e19a44ce2e34d2e4be4397193de6b2af0a
nibles1998 <longvynguyenhoang@gmail.com>
2020-07-02 12:29:54 +0700

    [Update] Fix task [Review] Route

--- routes/review.js
+++ routes/review.js
@@ -3,7 +3,7 @@ const reviewCtrl = require('../controllers').controller.Review;
 const authenticate = require('../middleware/authenticate');
 const role = require('../middleware/role');
 
-reviewRoute.get('/s', role.checkRoleQuery(["userid"], ["Admin", "Customer"], { read: true }), reviewCtrl.getMany);
+reviewRoute.get('/s', reviewCtrl.getMany);
 reviewRoute.get('/:_id', authenticate.authenticateJWT, role.checkRole(["Admin", "Customer"], { read: true }), reviewCtrl.getById);
 reviewRoute.post('/', authenticate.authenticateJWT, role.checkRole(["Admin", "Customer"], { create: true }), reviewCtrl.createData);
 reviewRoute.put('/:_id', authenticate.authenticateJWT, role.checkRole(["Admin", "Customer"], { up: true }), reviewCtrl.updateById);
